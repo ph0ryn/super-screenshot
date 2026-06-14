5808005232491b65423cc92b016edec771ab6840
ph0ryn <208665755+ph0ryn@users.noreply.github.com>
2026-06-14 15:45:11 +0900

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import oxlint from "eslint-plugin-oxlint";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  {
+    ignores: ["dist/**"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "class",
+          format: ["StrictPascalCase"],
+        },
+        {
+          selector: "variable",
+          format: ["strictCamelCase", "UPPER_CASE"],
+        },
+        {
+          selector: "parameter",
+          format: ["strictCamelCase"],
+        },
+      ],
+    },
+  },
+  oxlint.buildFromOxlintConfigFile("./oxlint.config.ts"),
+);
